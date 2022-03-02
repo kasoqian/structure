d5553baefec24b559d68f98f11e392a392813597
kasoqian <kasoqian@mugenart.io>
2022-03-02 20:00:23 +0800

    feat: 完成双向循环列表的前进后退功能,且代码量最小

--- list-circular.ts
+++ list-circular.ts
@@ -1,11 +1,11 @@
-// 循环链表
+// 双向循环链表
 export class node {
   element: any;
   next: any;
   previous: any;
 
   constructor(element: any) {
-    this.previous = null;
+    this.next = null;
     this.element = element;
     this.previous = null;
   }
@@ -14,6 +14,12 @@ export class node {
 export class LList {
   private head = new node("head");
 
+  constructor() {
+    this.head.previous = this.head;
+    this.head.next = this.head;
+  }
+
+  // 双向循环链表中不应该有push一说，这里只提供insert方法
   insert(newElement, item) {
     const newNode = new node(newElement);
     let curNode = this.head;
@@ -21,41 +27,11 @@ export class LList {
     while (curNode.element != item) {
       curNode = curNode.next;
     }
+
     newNode.previous = curNode;
-    newNode.next = curNode.next ? curNode.next : this.head;
+    newNode.next = curNode.next;
+    curNode.next.previous = newNode;
     curNode.next = newNode;
-  }
-
-  push(newElement) {
-    const newNode = new node(newElement);
-    let curNode = this.head;
-
-    while (curNode.next != null) {
-      curNode = curNode.next;
-    }
-
-    curNode.next = newNode;
-    newNode.previous = curNode;
-    newNode.next = this.head;
-  }
-
-  display() {
-    let curNode = this.head;
-
-    while (curNode != null) {
-      console.log(curNode.element);
-      curNode = curNode.next;
-    }
-  }
-
-  pop() {
-    let curNode = this.head;
-
-    while (curNode.next.next != null) {
-      curNode = curNode.next;
-    }
-
-    curNode.next = this.head;
   }
 
   remove(item) {
@@ -70,17 +46,46 @@ export class LList {
       : this.head;
     curNode.next.previous = curNode;
   }
+
+  advance(step: number) {
+    let curNode = this.head;
+
+    while (step > 0) {
+      curNode = curNode.next;
+      step--;
+    }
+
+    return curNode.element;
+  }
+
+  back(step: number) {
+    let curNode = this.head;
+
+    while (step > 0) {
+      curNode = curNode.previous;
+      step--;
+    }
+
+    return curNode.element;
+  }
 }
 
-// const list = new LList();
+const list = new LList();
 
-// list.push("good");
-// list.push("yes");
-// list.push("no");
-// list.insert("插入数据1", "yes");
-// list.insert("插入数据2", "no");
-// list.remove("yes");
-// list.push("我是啦啦队");
-// list.pop();
-// list.push("我负责收尾");
-// list.display();
+list.insert("good", "head");
+list.insert("test", "head");
+list.insert("yes", "good");
+list.insert("no", "yes");
+console.log(list.back(0));
+console.log(list.back(1));
+console.log(list.back(2));
+console.log(list.back(3));
+console.log(list.back(4));
+console.log(list.back(5));
+console.log("------------------");
+console.log(list.advance(0));
+console.log(list.advance(1));
+console.log(list.advance(2));
+console.log(list.advance(3));
+console.log(list.advance(4));
+console.log(list.advance(5));
