853508831968e81b9eebfea599db6ce6fcc69158
kasoqian <kasoqian@mugenart.io>
2022-03-04 13:03:44 +0800

    feat: 完成二叉树的创建,非平衡

--- src/structure/tree.ts
+++ src/structure/tree.ts
@@ -1,9 +1,52 @@
+// 普通非平衡二叉树
 class node {
   data: any;
   left: any;
   right: any;
-  constructor(element) {
+  constructor(element: number) {
     this.data = element;
-    this.left = this.right = null;
+    this.left = null;
+    this.right = null;
   }
 }
+
+class Tree {
+  private root = new node(14);
+
+  insert(element) {
+    let newNode = new node(element);
+
+    var current = this.root;
+    var parent;
+
+    while (current) {
+      parent = current;
+      if (element < current.data) {
+        current = current.left;
+        if (current == null) {
+          parent.left = newNode;
+          break;
+        }
+      } else {
+        current = current.right;
+        if (current == null) {
+          parent.right = newNode;
+          break;
+        }
+      }
+    }
+  }
+
+  display() {
+    return this.root;
+  }
+}
+
+const tree = new Tree();
+
+tree.insert(12);
+tree.insert(13);
+tree.insert(14);
+tree.insert(11);
+tree.insert(20);
+console.log(JSON.stringify(tree.display()));
